ba879605041e2ae6e38dd5533c247ddc4abf46c3
Remy Vuong <remy@vuong.fr>
2023-12-01 16:01:03 +0100

    Day 1, step 2 (WIP)

--- day/1/src/lib.rs
+++ day/1/src/lib.rs
@@ -1,4 +1,33 @@
+use std::fmt;
 use std::fs::read_to_string;
+
+enum Number {
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+}
+
+impl fmt::Display for Number {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Number::One => write!(f, "one"),
+            Number::Two => write!(f, "two"),
+            Number::Three => write!(f, "three"),
+            Number::Four => write!(f, "four"),
+            Number::Five => write!(f, "five"),
+            Number::Six => write!(f, "six"),
+            Number::Seven => write!(f, "seven"),
+            Number::Eight => write!(f, "eight"),
+            Number::Nine => write!(f, "nine"),
+        }
+    }
+}
 
 // order_asc: true=asc, false=desc
 fn get_digit(input: &&str, order_asc: bool) -> Option<u32> {
@@ -18,7 +47,81 @@ fn get_digit(input: &&str, order_asc: bool) -> Option<u32> {
     None
 }
 
+// Convert string numbers into int values
+// eg "[two]1[nine]" should be converted into "[2]1[9]"
+fn numerize_string_numbers(input: String) -> String {
+    // Can't use ".replace()" because the replace is not starting by left of the String,
+    // but by the pattern searched.
+    // input
+    //     .replace("one", "1")
+    //     .replace("two", "2")
+    //     .replace("three", "3")
+    //     .replace("four", "4")
+    //     .replace("five", "5")
+    //     .replace("six", "6")
+    //     .replace("seven", "7")
+    //     .replace("eight", "8")
+    //     .replace("nine", "9")
+    // result:      "eightwothree"  -> "eigh2three" (2) -> "eigh23"
+    // expected:    "eightwothree"  -> "8wothree" (8)   -> "8wo3"
+    // So, we must instead parse the String from left to right, and replace case by case.
+
+    let mut output: String = input;
+    let mut output_len = output.len();
+    let numbers = vec![
+        Number::One,
+        Number::Two,
+        Number::Three,
+        Number::Four,
+        Number::Five,
+        Number::Six,
+        Number::Seven,
+        Number::Eight,
+        Number::Nine,
+    ];
+
+    let mut i = 0;
+    while i < output_len {
+        // Match each substring starting at that index, with number as words
+        for n in numbers.iter() {
+            let number = n.to_string();
+            let number_length = number.len();
+
+            unsafe {
+                let end = i + number_length;
+                let slice = output.get_unchecked(i..end);
+
+                if slice == number {
+                    let before = output.get_unchecked(0..i);
+                    let after = output.get_unchecked(end..);
+                    let number_value = match n {
+                        Number::One => 1,
+                        Number::Two => 2,
+                        Number::Three => 3,
+                        Number::Four => 4,
+                        Number::Five => 5,
+                        Number::Six => 6,
+                        Number::Seven => 7,
+                        Number::Eight => 8,
+                        Number::Nine => 9,
+                    };
+
+                    output = format!("{}{}{}", before, number_value, after);
+                    output_len = output.len();
+                }
+            }
+        }
+
+        i += 1;
+    }
+
+    output
+}
+
 pub fn get_two_digit_number(input: &str) -> u32 {
+    let input = numerize_string_numbers(input.to_string());
+    let input = input.as_str();
+
     let first_digit = get_digit(&input, true);
     let last_digit = get_digit(&input, false);
 
--- day/1/tests/trebuchet.rs
+++ day/1/tests/trebuchet.rs
@@ -31,12 +31,68 @@ fn test_calibration_value_3() {
 }
 
 #[test]
+fn test_calibration_value_4() {
+    let input_word = "two1nine";
+    let result = trebuchet::get_two_digit_number(input_word);
+
+    assert_eq!(result, 29);
+}
+
+#[test]
+fn test_calibration_value_5() {
+    let input_word = "eightwothree";
+    let result = trebuchet::get_two_digit_number(input_word);
+
+    assert_eq!(result, 83);
+}
+
+#[test]
+fn test_calibration_value_6() {
+    let input_word = "abcone2threexyz";
+    let result = trebuchet::get_two_digit_number(input_word);
+
+    assert_eq!(result, 13);
+}
+
+#[test]
+fn test_calibration_value_7() {
+    let input_word = "xtwone3four";
+    let result = trebuchet::get_two_digit_number(input_word);
+
+    assert_eq!(result, 24);
+}
+
+#[test]
+fn test_calibration_value_8() {
+    let input_word = "4nineeightseven2";
+    let result = trebuchet::get_two_digit_number(input_word);
+
+    assert_eq!(result, 42);
+}
+
+#[test]
+fn test_calibration_value_9() {
+    let input_word = "zoneight234";
+    let result = trebuchet::get_two_digit_number(input_word);
+
+    assert_eq!(result, 14);
+}
+
+#[test]
+fn test_calibration_value_10() {
+    let input_word = "7pqrstsixteen";
+    let result = trebuchet::get_two_digit_number(input_word);
+
+    assert_eq!(result, 76);
+}
+
+#[test]
 #[ignore]
 fn test_input_row_value_0() {
     let input_word = "fivepqxlpninevh2xxsnsgg63pbvdnqptmg";
     let result = trebuchet::get_two_digit_number(input_word);
 
-    assert_eq!(result, 23);
+    assert_eq!(result, 53);
 }
 
 #[test]
@@ -45,7 +101,7 @@ fn test_input_row_value_1() {
     let input_word = "eight8zlctbmsixhrvbpjb84nnmlcqkzrsix";
     let result = trebuchet::get_two_digit_number(input_word);
 
-    assert_eq!(result, 84);
+    assert_eq!(result, 86);
 }
 
 #[test]
@@ -54,5 +110,5 @@ fn test_input_row_value_2() {
     let input_word = "hkxqfrqmsixfplbkpkdfzzszjxmdjtdkjlprrvr3gghlrqckqtbng";
     let result = trebuchet::get_two_digit_number(input_word);
 
-    assert_eq!(result, 33);
+    assert_eq!(result, 63);
 }
